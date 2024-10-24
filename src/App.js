b9fa5da57084ec4132be1f0e16727f00f9044161
Emad Albalas <ealbalas3@gatech.edu>
2024-10-23 22:57:18 -0700

    fixed it so that the filter is not case sensitive

--- src/App.js
+++ src/App.js
@@ -25,7 +25,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) =>{}} />
+        <input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) =>{
+          const searchString = event.target.value.toLowerCase();
+          const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchString);
+          });
+
+          this.setState(() => {
+            return {monsters: filteredMonsters};
+          }, () => {
+            console.log(this.state);
+          })
+        }} />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}>
