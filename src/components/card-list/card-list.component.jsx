4bdc191a4ba11ac21b6c03101cc1027235482c46
Emad Albalas <ealbalas3@gatech.edu>
2024-10-27 16:19:21 -0700

    converted card-list into functional function instead of class

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,14 @@
-import { Component } from "react";
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
-
-        return (
-        <div className="card-list">
-            {monsters.map(monster => {
-                const { id, name, email } = monster;
-                return (
-                    <Card id={id} name={name} email={email} />                
-                )})}
-        </div>);
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map(monster => {
+            const { id, name, email } = monster;
+            return (
+                <Card id={id} name={name} email={email} />                
+            )})}
+    </div>
+);
 
 export default CardList;
