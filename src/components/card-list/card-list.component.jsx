53897f5b82fc8612bcc596534889ee9e2fbfc5dd
Emad Albalas <ealbalas3@gatech.edu>
2024-10-27 12:06:17 -0700

    starting to break out components so i can generalize them and reuse them

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return <div>Hello I am the CardList component</div>;
+    }
+}
+
+export default CardList;
